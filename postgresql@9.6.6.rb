6e76c871ac04ef2730270a9e4e830d20316c05fe
Brice Texier <burisu@oneiros.fr>
2018-08-20 21:58:59 +0200

    Update PostgreSQL 9.6.6 with core Formula

--- postgresql@9.6.6.rb
+++ postgresql@9.6.6.rb
@@ -1,89 +1,150 @@
 class PostgresqlAT966 < Formula
-  desc "Relational database management system"
+  desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  version = "9.6.6"
-  url "https://ftp.postgresql.org/pub/source/v#{version}/postgresql-#{version}.tar.bz2"
+  url "https://ftp.postgresql.org/pub/source/v9.6.6/postgresql-9.6.6.tar.bz2"
   sha256 "399cdffcb872f785ba67e25d275463d74521566318cfef8fe219050d063c8154"
 
-  head do
-    url "https://git.postgresql.org/git/postgresql.git", :branch => "REL9_6_STABLE"
-
-    depends_on "docbook-xsl" => :build
-    depends_on "open-sp" => :build
-    depends_on "petere/sgml/docbook-dsssl" => :build
-    depends_on "petere/sgml/docbook-sgml" => :build
-    depends_on "petere/sgml/openjade" => :build
+  bottle do
+    sha256 "1dd8ecf2fa8330b7d04d389e0afd8aef4a9f5dc426ee3f9630573f43b9ab7aae" => :high_sierra
+    sha256 "c7836cde4468cbd8a52fc903f51dc34ece601abf081267fb7cbd564fde6cefce" => :sierra
+    sha256 "af200e4e87de99914d197b43b0bb5db90a34cb23abafc584c357610874a30e7b" => :el_capitan
   end
 
   keg_only :versioned_formula
 
-  deprecated_option "enable-cassert" => "with-cassert"
-  option "with-cassert", "Enable assertion checks (for debugging)"
+  option "without-perl", "Build without Perl support"
+  option "without-tcl", "Build without Tcl support"
+  option "with-dtrace", "Build with DTrace support"
+  option "with-python", "Enable PL/Python3 (incompatible with --with-python@2)"
+  option "with-python@2", "Enable PL/Python2"
 
-  depends_on "e2fsprogs"
-  depends_on "gettext"
-  depends_on "openldap"
+  deprecated_option "no-perl" => "without-perl"
+  deprecated_option "no-tcl" => "without-tcl"
+  deprecated_option "enable-dtrace" => "with-dtrace"
+  deprecated_option "with-python3" => "with-python"
+
   depends_on "openssl"
   depends_on "readline"
-  depends_on "tcl-tk"
+
+  depends_on "python" => :optional
+  depends_on "python@2" => :optional
+
+  fails_with :clang do
+    build 211
+    cause "Miscompilation resulting in segfault on queries"
+  end
 
   def install
+    # avoid adding the SDK library directory to the linker search path
+    ENV["XML2_CONFIG"] = "xml2-config --exec-prefix=/usr"
+
+    ENV.prepend "LDFLAGS", "-L#{Formula["openssl"].opt_lib} -L#{Formula["readline"].opt_lib}"
+    ENV.prepend "CPPFLAGS", "-I#{Formula["openssl"].opt_include} -I#{Formula["readline"].opt_include}"
+
     args = %W[
+      --disable-debug
       --prefix=#{prefix}
-      --enable-dtrace
-      --enable-nls
+      --datadir=#{pkgshare}
+      --libdir=#{lib}
+      --sysconfdir=#{prefix}/etc
+      --docdir=#{doc}
+      --enable-thread-safety
       --with-bonjour
       --with-gssapi
       --with-ldap
+      --with-openssl
+      --with-pam
       --with-libxml
       --with-libxslt
-      --with-openssl
-      --with-uuid=e2fs
-      --with-pam
-      --with-perl
-      --with-python
-      --with-tcl
-      XML2_CONFIG=:
     ]
 
-    # Add include and library directories of dependencies, so that
-    # they can be used for compiling extensions.  Superenv does this
-    # when compiling this package, but won't record it for pg_config.
-    deps = %w[gettext openldap openssl readline tcl-tk]
-    with_includes = deps.map { |f| Formula[f].opt_include }.join(":")
-    with_libraries = deps.map { |f| Formula[f].opt_lib }.join(":")
-    args << "--with-includes=#{with_includes}"
-    args << "--with-libraries=#{with_libraries}"
+    args << "--with-perl" if build.with? "perl"
 
-    args << "--enable-cassert" if build.with? "cassert"
+    which_python = nil
+    if build.with?("python") && build.with?("python@2")
+      odie "Cannot provide both --with-python and --with-python@2"
+    elsif build.with?("python") || build.with?("python@2")
+      args << "--with-python"
+      which_python = which(build.with?("python") ? "python3" : "python2.7")
+    end
+    ENV["PYTHON"] = which_python
 
-    extra_version = ""
-    extra_version += "+git" if build.head?
-    extra_version += " (Homebrew petere/postgresql)"
-    args << "--with-extra-version=#{extra_version}"
+    # The CLT is required to build Tcl support on 10.7 and 10.8 because
+    # tclConfig.sh is not part of the SDK
+    if build.with?("tcl") && (MacOS.version >= :mavericks || MacOS::CLT.installed?)
+      args << "--with-tcl"
 
-    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+      if File.exist?("#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework/tclConfig.sh")
+        args << "--with-tclconfig=#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework"
+      end
+    end
+
+    args << "--enable-dtrace" if build.with? "dtrace"
+    args << "--with-uuid=e2fs"
 
     system "./configure", *args
-    system "make", "install-world"
+    system "make"
+    system "make", "install-world", "datadir=#{pkgshare}",
+                                    "libdir=#{lib}",
+                                    "pkglibdir=#{lib}"
+  end
+
+  def post_install
+    (var/"log").mkpath
+    (var/name).mkpath
+    unless File.exist? "#{var}/#{name}/PG_VERSION"
+      system "#{bin}/initdb", "#{var}/#{name}"
+    end
   end
 
   def caveats; <<~EOS
-    To use this PostgreSQL installation, do one or more of the following:
+    If builds of PostgreSQL 9 are failing and you have version 8.x installed,
+    you may need to remove the previous version first. See:
+      https://github.com/Homebrew/legacy-homebrew/issues/2510
 
-    - Call all programs explicitly with #{opt_prefix}/bin/...
-    - Add #{opt_bin} to your PATH
-    - brew link -f #{name}
-    - Install the postgresql-common package
+    To migrate existing data from a previous major version (pre-9.0) of PostgreSQL, see:
+      https://www.postgresql.org/docs/9.6/static/upgrading.html
 
-    To access the man pages, do one or more of the following:
-    - Refer to them by their full path, like `man #{opt_share}/man/man1/psql.1`
-    - Add #{opt_share}/man to your MANPATH
-    - brew link -f #{name}
-    EOS
+    To migrate existing data from a previous minor version (9.0-9.5) of PostgreSQL, see:
+      https://www.postgresql.org/docs/9.6/static/pgupgrade.html
+
+      You will need your previous PostgreSQL installation from brew to perform `pg_upgrade`.
+        Do not run `brew cleanup postgresql@9.6` until you have performed the migration.
+  EOS
+  end
+
+  plist_options :manual => "pg_ctl -D #{HOMEBREW_PREFIX}/var/postgresql@9.6 start"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/postgres</string>
+        <string>-D</string>
+        <string>#{var}/#{name}</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/#{name}.log</string>
+    </dict>
+    </plist>
+  EOS
   end
 
   test do
-    system "#{bin}/initdb", "pgdata"
+    system "#{bin}/initdb", testpath/"test"
+    assert_equal pkgshare.to_s, shell_output("#{bin}/pg_config --sharedir").chomp
+    assert_equal lib.to_s, shell_output("#{bin}/pg_config --libdir").chomp
+    assert_equal lib.to_s, shell_output("#{bin}/pg_config --pkglibdir").chomp
   end
 end
